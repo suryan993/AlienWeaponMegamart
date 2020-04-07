9ab5f49567933dfe603e8e02012ad66bfb38ab5b
Mansoor Patan <1955mars@gmail.com>
2020-04-07 01:07:00 -0400

    Mouse control + Behaviors(weapon, projectile) added

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,7 +8,10 @@ public class Player : MonoBehaviour
     Rigidbody2D rb;
     // Start is called before the first frame update
     bool holdingWeapon;
-    GameObject weapon;
+    GameObject currWeapon;
+    GameObject prevWeapon;
+
+    Transform mTransform;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -24,24 +27,58 @@ public class Player : MonoBehaviour
 
         force *= moveForce;
         //Debug.Log(transform.position.y + moveY);
+        Vector3 mouseDir = GetNormalizedDirectionToMouse();
+        transform.rotation = Quaternion.LookRotation(Vector3.forward, mouseDir);
 
         rb.AddForce(force);
 
         if (holdingWeapon)
         {
-            weapon.transform.position = transform.position;
+            currWeapon.transform.position = transform.position;
+            currWeapon.transform.rotation = transform.rotation;
         }
 
+        if(Input.GetMouseButtonDown(0))// If left button is pressed
+        {
+            currWeapon.GetComponent<Weapon>().Fire(mouseDir);
+        }
     }
+
+   
+    public Vector3 GetNormalizedDirectionToMouse()
+    {
+        Vector3 viewPortActual, viewPortDelta, directionToMouse;
+
+        // Calculate a direction based on the Mouse Position (using Viewport)
+        viewPortActual = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+        viewPortDelta = new Vector3(0.5f, 0.5f, 0); // Detract Half the screen
+        directionToMouse = viewPortActual - viewPortDelta; // Caculate Direction
+        return directionToMouse.normalized; // Return normalized Direction
+    }
+
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        //Debug.Log("Trigger enter " + collision.gameObject.tag);
         if (collision.gameObject.tag.Equals("Weapon"))  //get bread, reduce cleanliness
         {
+            if (!collision.gameObject.GetComponent<Weapon>().isPickable)
+                return;
+
             holdingWeapon = true;
-            weapon = collision.gameObject;
+
+            if (currWeapon)
+            {
+                currWeapon.GetComponent<Weapon>().isPickable = false;
+                currWeapon.GetComponent<BoxCollider2D>().enabled = true;
+            }
+
+            prevWeapon = currWeapon;
+            currWeapon = collision.gameObject;
+
+            currWeapon.GetComponent<BoxCollider2D>().enabled = false;
             //gameObject.transform.GetChild(0).GetComponent<SpriteRenderer>().sprite = collision.gameObject.GetComponent<SpriteRenderer>().sprite;
-            collision.gameObject.GetComponent<BoxCollider2D>().enabled = false;
         }
     }
+
 }
---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public float lifeTime = 5.0f;
+    public float speed = 10f;
+    public float acceleration = 0f;
+    public Vector3 firedDirection = Vector3.zero;
+
+    Animator anim;
+
+    Vector3 velocity = Vector3.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        velocity = speed * firedDirection;
+        anim = GetComponent<Animator>();
+        anim.SetBool("Firing", true);
+        Destroy(gameObject, lifeTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(firedDirection != Vector3.zero)
+        {
+            velocity += acceleration * firedDirection * Time.deltaTime;
+            transform.position += velocity * Time.deltaTime;
+        }
+        else
+        {
+            Debug.Log("Direction of fire is not set");
+        }
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Projectile Trigger enter " + collision.gameObject.tag);
+        if (collision.gameObject.tag.Equals("targetExplode"))
+        {
+            Debug.Log("Entering Player");
+
+            Destroy(gameObject);
+        }
+    }
+
+}
